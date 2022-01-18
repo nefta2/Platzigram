0bf56d977db29ea0fa182076ace714a0314c67d8
Diego <diego.burgos@utp.ac.pa>
2022-01-18 00:30:05 -0500

    Posts

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,6 +1,8 @@
 """Platzigram URLs module."""
 #Django
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from platzigram import views as local_views
@@ -12,5 +14,6 @@ urlpatterns = [
     path('hello_world/', local_views.hello_world),
     path('sorted/', local_views.sort_integers),
     path('hi/<str:name>/<int:age>/', local_views.say_hi),
+
     path('posts/', posts_views.list_posts),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/models.py
+++ posts/models.py
@@ -1,31 +1,22 @@
 """Posts models."""
 
 #Django 
-from enum import unique
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class User (models.Model):
-    """User model."""
+class Post(models.Model):
+    """Post model."""
 
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey('users.Profile', on_delete=models.CASCADE)
 
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-
-    is_admin = models.BooleanField(default=False)
-
-    bio = models.TextField(blank=True)
-    
-    birthdate = models.DateField(blank=True, null=True)
+    title = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='posts/photos')
 
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
-    country = models.CharField(max_length=100, blank= True, null=True)
-    city = models.CharField(max_length=100, blank= True, null=True)
-    
     def __str__(self):
-        """Return email."""
-        return self.email
+        """Return title and username."""
+        return '{} by @{}'.format(self.title, self.user.username)
