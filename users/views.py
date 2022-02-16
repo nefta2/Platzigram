63ca3d3754bc5588acd06f74770c9bac1a84af90
Diego <diego.burgos@utp.ac.pa>
2022-02-15 19:46:49 -0500

    Middleware

--- users/views.py
+++ users/views.py
@@ -13,6 +13,41 @@ from django.db.utils import IntegrityError
 from django.contrib.auth.models import User
 from users.models import Profile
 
+# Forms
+from users.forms import ProfileForm
+
+
+def update_profile(request):
+    """Update a user's profile view."""
+    profile = request.user.profile
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            data = form.cleaned_data
+
+            profile.website = data['website']
+            profile.phone_number = data['phone_number']
+            profile.biography = data['biography']
+            profile.picture = data['picture']
+            profile.save()
+
+            return redirect (update_profile)
+
+    else:
+        form = ProfileForm()
+
+    return render(
+        request=request, 
+        template_name='users/update_profile.html',
+        context={
+            'profile': profile,
+            'user': request.user,
+            'form': form
+        }
+    )
+
+
 def login_view(request):
     """Login view."""
     if request.method == 'POST':
@@ -27,6 +62,7 @@ def login_view(request):
             return render(request, 'users/login.html', {'error': 'Invalid username and password'})
 
     return render(request, 'users/login.html')
+
 def signup(request):
     """Sign up view."""
     if request.method == "POST":
