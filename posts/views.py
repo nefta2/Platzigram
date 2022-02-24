f4ba2670005ae854d20d359918be9c163029714f
Diego <diego.burgos@utp.ac.pa>
2022-02-23 22:19:55 -0500

    Forms

--- posts/views.py
+++ posts/views.py
@@ -1,46 +1,41 @@
 """Posts views."""
 
 # Django
+import profile
 from turtle import width
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Utilities
-from datetime import datetime
+# forms
+from posts.forms import PostForm
 
-posts = [
-    {
-        'title': 'Sancocho de Patio',
-        'user': {
-            'name': 'Dundunsua',
-            'picture': 'https://i.postimg.cc/TwXCrM7k/49906995-355460195005805-3926332708066558657-n1-1.jpg',
-        },
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Barranquilla_-_Sancocho_de_mondongo.jpg/800px-Barranquilla_-_Sancocho_de_mondongo.jpg',
-    },
-    {
-        'title': 'Uñas de curundú',
-        'user': {
-            'name': 'Roxana',
-            'picture': 'https://i.postimg.cc/fbYvWqzY/aohqw-ssmk9.jpg',
-        },
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://www.tuenlinea.com/wp-content/uploads/2018/08/Nail-Art-inspirado-chicas-s%C3%BAper-feministas.jpg',
-    },
-    {
-        'title': 'Mi rosa',
-        'user': {
-            'name': 'Nito Cortizo',
-            'picture': 'https://i.postimg.cc/Fs2cfbnp/avoth-5ymg5.jpg',
-        }, 
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://live.staticflickr.com/43/107602069_0d6211e5f1_c.jpg',
-    },
-]
-
+# Models
+from posts.models import Post
 
 
 @login_required
 def list_posts(request):
     """List existing posts."""
+    posts = Post.objects.all().order_by('-created')
     return render(request, 'posts/feed.html', {'posts': posts})
+
+@login_required
+def create_post(request):
+    """Create new post view."""
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('feed')
+
+    else:
+        form = PostForm()
+    return render(
+        request= request,
+        template_name='posts/new.html',
+        context={
+            'form': form,
+            'user': request.user,
+            'profile': request.user.profile
+        }
+    )
--- users/views.py
+++ users/views.py
@@ -6,17 +6,10 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
-#Exception
-from django.db.utils import IntegrityError
-
-# Models
-from django.contrib.auth.models import User
-from users.models import Profile
-
 # Forms
-from users.forms import ProfileForm
+from users.forms import ProfileForm, SignupForm
 
-
+@login_required
 def update_profile(request):
     """Update a user's profile view."""
     profile = request.user.profile
@@ -65,30 +58,19 @@ def login_view(request):
 
 def signup(request):
     """Sign up view."""
-    if request.method == "POST":
-        username = request.POST ['username']
-        passwd = request.POST ['passwd']
-        passwd_confirmation = request.POST ['passwd_confirmation']
-
-        if passwd != passwd_confirmation:
-            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
-        
-        try:
-            user = User.objects.create_user(username=username, password=passwd)
-        except IntegrityError:
-            return render(request, 'users/signup.html', {'error': 'Username is already in used'})
-       
-        user.first_name = request.POST['first_name']
-        user.last_name = request.POST['last_name']
-        user.email = request.POST['email']
-        user.save()
-
-        profile = Profile(user=user)
-        profile.save()
-
-        return redirect ('login')
-
-    return render(request, 'users/signup.html')
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignupForm()
+    
+    return render(
+        request=request,
+        template_name='users/signup.html',
+        context={'form': form},
+    )
 
 
 @login_required
